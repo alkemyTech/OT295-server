9d41f2f116bcb12e1731cc9690176909fb3f34c5
leramp <leandroramp@gmail.com>
2022-09-16 16:39:47 +0200

    UserEntity and UserRepository creaded

---
+++ src/main/java/com/alkemy/ong/domain/UserEntity.java
@@ -0,0 +1,57 @@
+package com.alkemy.ong.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql="UPDATE users SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @JoinColumn(name= "role_id", insertable = false, updatable = false)
+    private RoleEntity role;
+
+    @Column (name="role_id", nullable = false)
+    private Long roleId;
+
+    private boolean deleted = Boolean.FALSE;
+
+    @CreationTimestamp
+    @Column(name = "CREATE_TIMESTAMP", updatable = false)
+    private Timestamp createTimestamp;
+}
+//        firstName: VARCHAR NOT NULL
+//        lastName: VARCHAR NOT NULL
+//        email: VARCHAR UNIQUE NOT NULL
+//        password: VARCHAR NOT NULL
+//        photo: VARCHAR NULLABLE
+//        roleId: Clave foranea hacia ID de Role
+//        timestamps y softDelete
