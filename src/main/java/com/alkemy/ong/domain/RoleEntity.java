6d3b214cc5b6f2f716310ef3cd9f19f39ac8cfdf
Francisco Arana <panchoarana2014@gmail.com>
2022-09-16 10:05:17 -0300

    OT295-17: Create model & repository role

---
+++ src/main/java/com/alkemy/ong/domain/RoleEntity.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    @CreatedDate
+    @Column(columnDefinition = "timestamp")
+    private Timestamp timestamp;
+
+}
