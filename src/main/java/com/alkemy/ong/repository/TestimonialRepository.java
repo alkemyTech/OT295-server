474ebf418dc65c7484c381323c0be574bde3d992
alanfv22 <alanfveron@gmail.com>
2022-09-16 20:51:14 -0300

    OT295-21 create repository Testimonial

---
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestimonialRepository  extends JpaRepository<TestimonialRepository, Long> {
+}
