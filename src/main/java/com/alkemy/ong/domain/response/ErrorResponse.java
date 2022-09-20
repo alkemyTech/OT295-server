0c9e461ff0c5c4d3edbc945eab086ce6f92da388
Braian Marquez <braianmarq95@gmail.com>
2022-09-20 00:29:07 -0300

    OT295-38: modify the ErrorResponse class

--- src/main/java/com/alkemy/ong/domain/response/ErrorResponse.java
+++ src/main/java/com/alkemy/ong/domain/response/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.domain.response;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 public class ErrorResponse {
 
 
