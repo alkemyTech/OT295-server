06a11713004680517699eafbe7869f68300a3216
Tomas Molnar <tomas.molnar@ayi.group>
2022-09-21 12:58:09 -0300

    Modificaciones codeReview Retro

--- src/main/java/com/alkemy/ong/service/OrganizationService.java
+++ src/main/java/com/alkemy/ong/service/OrganizationService.java
@@ -4,11 +4,12 @@ import com.alkemy.ong.domain.dto.OrganizationBasicDTO;
 import com.alkemy.ong.domain.request.OrganizationRequest;
 import com.alkemy.ong.domain.response.OrganizationResponse;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrganizationService {
 
-    OrganizationBasicDTO getOrganization(UUID id);
-    
+    List<OrganizationBasicDTO> getOrganizations();
+
     OrganizationResponse update(OrganizationRequest organizationRequest);
 }
