3efcacd229c7be199597f2337d2cc4bece88287a
Daniel-Zemanate <zemanateaguirre@gmail.com>
2022-09-26 10:06:44 -0500

    OT295-40 Reusable endpoint for uploading files into S3, additional endpoint for deletion

---
+++ src/main/java/com/alkemy/ong/controller/BucketController.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.controller;
+
+
+import com.alkemy.ong.service.impl.AmazonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/storage")
+public class BucketController {
+
+    private AmazonClient amazonClient;
+
+    @Autowired
+    BucketController(AmazonClient amazonClient) {
+        this.amazonClient = amazonClient;
+    }
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestPart(value = "file") MultipartFile file) {
+        return this.amazonClient.uploadFile(file);
+    }
+
+    @DeleteMapping("/deleteFile")
+    public String deleteFile(@RequestPart(value = "url") String fileUrl) {
+        return this.amazonClient.deleteFileFromS3Bucket(fileUrl);
+    }
+}
