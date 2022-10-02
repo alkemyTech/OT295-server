312801c68ec3c1a43c57c4a945a0df3c80491570
alanfv22 <alanfveron@gmail.com>
2022-10-02 17:57:19 -0300

    OT295-69

--- src/main/java/com/alkemy/ong/controller/MemberController.java
+++ src/main/java/com/alkemy/ong/controller/MemberController.java
@@ -2,21 +2,18 @@ package com.alkemy.ong.controller;
 
 import com.alkemy.ong.domain.dto.MemberDTO;
 import com.alkemy.ong.domain.request.MemberRequest;
+import com.alkemy.ong.domain.response.MemberResponse;
+import com.alkemy.ong.repository.MemberRepository;
 import com.alkemy.ong.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.UUID;
-import com.alkemy.ong.domain.response.MemberResponse;
-import com.alkemy.ong.repository.MemberRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 
 @RestController
@@ -30,6 +27,12 @@ public class MemberController {
     public MemberController(MemberRepository memberRepository, MemberService service) {
         this.memberRepository = memberRepository;
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MemberResponse>> getMembers() {
+        List<MemberResponse> members = service.getMembers();
+        return ResponseEntity.ok().body(members);
     }
 
     @PutMapping("/{id}")
