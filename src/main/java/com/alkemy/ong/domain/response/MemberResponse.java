42f7b88bd752a8dbaeae591889845c54c6dd48cf
Braian Marquez <braianmarq95@gmail.com>
2022-10-15 11:24:41 -0300

    OT295-83: Add Pagination to Members

--- src/main/java/com/alkemy/ong/domain/response/MemberResponse.java
+++ src/main/java/com/alkemy/ong/domain/response/MemberResponse.java
@@ -1,11 +1,15 @@
 package com.alkemy.ong.domain.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class MemberResponse {
+
     private String name;
 
     private String facebookUrl;
