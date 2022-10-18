5d1a3b17097e8ee431a0dd0254026df097e94328
leramp <leandroramp@gmail.com>
2022-10-18 22:55:06 +0200

    feature/OT295-89 Testimonial documentation added

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -5,6 +5,11 @@ import com.alkemy.ong.domain.response.MemberPageResponse;
 import com.alkemy.ong.domain.response.TestimonialPageResponse;
 import com.alkemy.ong.domain.response.TestimonialResponse;
 import com.alkemy.ong.service.TestimonialService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,12 +28,15 @@ public class TestimonialController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public ResponseEntity<TestimonialResponse> createTestimonial(@Valid @RequestBody TestimonialRequest request){
+    @Operation(summary = "Create a testimonial through a TestimonialRequest", description = "This endpoint inserts a new testimonial")
+    public ResponseEntity<TestimonialResponse> createTestimonial(@Parameter(description = "It is a body in JSON format with the entity's atributes")@Valid @RequestBody TestimonialRequest request){
         return ResponseEntity.ok().body(testimonialService.saveTestimonial(request));
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping("/{id}")
+    @Operation(summary = "Update information about a testimonial", description = "This endpoint requires a testimonial ID and Body to perform update of information")
+    @Parameter(name ="id", description = "UUID type for testimonial id", example="00000000-0000-0000-0000-000000000001")
     public ResponseEntity<TestimonialResponse> updateTestimonial(@Valid @RequestBody TestimonialRequest request, @PathVariable UUID id){
         TestimonialResponse response = testimonialService.update(request, id);
         return ResponseEntity.ok().body(response);
@@ -36,12 +44,16 @@ public class TestimonialController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete information about a testimonial", description = "This endpoint requires a testimonial ID to perform delete of information")
+    @Parameter(name ="id", description = "UUID type for testimonial id", example="00000000-0000-0000-0000-000000000001")
     public ResponseEntity<Object> deleteTestimonial(@PathVariable UUID id){
         this.testimonialService.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
     @PreAuthorize("hasAnyRole('ROLE_USER'),('ROLE_ADMIN')")
     @GetMapping("/page/{page}")
+    @Operation(summary = "Get a paginated list of testimonials with minimal information", description = "This endpoint retrieves a paginated list of testimonials containing minimal information")
+    @Parameter(name ="page", description = "It is the page number to access", example="1")
     public ResponseEntity<TestimonialPageResponse> getAll(@PathVariable Integer page){
         TestimonialPageResponse testimonial = testimonialService.getAllTestimonials(page);
         return ResponseEntity.ok().body(testimonial);
